ff253cc8b2f2dcc66eac8eca287e3994377d284b
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-04-13 18:03:12 +0530

    db scripts

---
+++ hsql.sh
@@ -0,0 +1,3 @@
+cd /Users/apple/Downloads/hsqldb-2.6.0/hsqldb/lib
+java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:hibernatedb --dbname.0 mydb
+
---
+++ hsqlplus.sh
@@ -0,0 +1,3 @@
+cd /Users/apple/hsqldb-2.6.1/hsqldb/lib
+java -jar hsqldb.jar
+
