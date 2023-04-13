6ba10ae571b09320b631fe01e36ae3d33638ad78
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-04-13 17:53:25 +0530

    with insert function

--- EmployeeRepositoryImpl.java
+++ EmployeeRepositoryImpl.java
@@ -1,5 +1,10 @@
 package com.sbi.repo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -13,10 +18,7 @@ import com.sbi.pojo.Employee;
 @Repository("empRepo")
 public class EmployeeRepositoryImpl implements EmployeeRepository {
 
-	
 	DataSource dataSource; //i want this to be auto injected
-	
-	
 	
 	public EmployeeRepositoryImpl(@Qualifier("myds1") DataSource dataSource) {
 		super();
@@ -29,7 +31,18 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 	public void addEmployee(Employee emp) {
 		// TODO Auto-generated method stub
 		System.out.println("adding the employee.....dataSource : "+dataSource);
-
+		try {
+			Connection conn = dataSource.getConnection(); //DriverManager.registerDriver is done internally
+			PreparedStatement statement = conn.prepareStatement("INSERT INTO EMPLOYEE VALUES (?,?,?)");
+			statement.setInt(1, emp.getEmployeeNumber());
+			statement.setString(2, emp.getEmployeeName());
+			statement.setFloat(3, emp.getEmployeeSalary());
+			statement.executeUpdate();
+			System.out.println("New employee record inserted...");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
